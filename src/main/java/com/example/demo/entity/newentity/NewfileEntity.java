512f9c8cc721e146855c8be83e59c77d8710866b
bala-amypo <bala@amypo.in>
2025-12-17 08:22:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/newentity/NewfileEntity.java
+++ src/main/java/com/example/demo/entity/newentity/NewfileEntity.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
+@Table(name="Student")
 
 public class NewfileEntity {
     @Id
@@ -17,11 +18,8 @@ public class NewfileEntity {
     private String name;
     @NotBlank(message = "no blank allowed")
     @Email(message="Invalid format")
-    private String email;public NewfileEntity(
-            Long id,
-            @NotBlank(message = "should not contain spaces") String name,
-            @NotBlank(message = "no blank allowed")
-            @Email(message = "invalid format") String email) {
+    private String email;public NewfileEntity(Long id,@NotBlank(message = "should not contain spaces") String name,
+            @NotBlank(message = "no blank allowed") @Email(message = "invalid format") String email) {
 
         this.id = id;
         this.name = name;
