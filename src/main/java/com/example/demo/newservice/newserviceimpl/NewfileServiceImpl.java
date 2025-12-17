3c5397d8749355f4bc3e6e240534dfa19d4509ce
bala-amypo <bala@amypo.in>
2025-12-17 09:27:12 +0000

    User commit

--- src/main/java/com/example/demo/newservice/newserviceimpl/NewfileServiceImpl.java
+++ src/main/java/com/example/demo/newservice/newserviceimpl/NewfileServiceImpl.java
@@ -9,28 +9,28 @@ import com.example.demo.newservice.NewfileService;
 public class NewfileServiceImpl implements NewfileService {
 
     private final NewfileRepo rep;
+
+    public NewfileServiceImpl(){
+        this.rep=rep;
+    }
     @Override
     public NewfileEntity savedata(NewfileEntity newfile) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'savedata'");
+        
     }
 
     @Override
     public NewfileEntity getidval(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getidval'");
+        
     }
 
     @Override
     public List<NewfileEntity> getall() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getall'");
+       
     }
 
     @Override
     public NewfileEntity update(Long id, NewfileEntity newfile) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+       
     }
     @Override
 public void delete(Long id) {
