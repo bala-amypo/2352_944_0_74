d0b8b85a72a77867566f53d059a07aa0c18d483f
bala-amypo <bala@amypo.in>
2025-12-17 09:47:11 +0000

    User commit

--- src/main/java/com/example/demo/newservice/newserviceimpl/NewfileServiceImpl.java
+++ src/main/java/com/example/demo/newservice/newserviceimpl/NewfileServiceImpl.java
@@ -30,15 +30,21 @@ public class NewfileServiceImpl implements NewfileService {
 
     @Override
     public List<NewfileEntity> getall() {
+        return rep.findAll();
        
     }
 
     @Override
     public NewfileEntity update(Long id, NewfileEntity newfile) {
+        NewfileEntity existing =getidval(id);
+        existing.setName(newfile.getName());
+        existing.setEmail(newfile.getEmail());
+        return rep.save(existing);
        
     }
     @Override
 public void delete(Long id) {
+    delete(Long id);
     
 }
 
