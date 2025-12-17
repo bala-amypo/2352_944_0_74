080a00a94c61245d4746a50b6b6965a8663f1e51
bala-amypo <bala@amypo.in>
2025-12-17 09:02:12 +0000

    User commit

--- src/main/java/com/example/demo/newservice/NewfileService.java
+++ src/main/java/com/example/demo/newservice/NewfileService.java
@@ -1,11 +1,12 @@
 package com.example.demo.newservice;
 
+import  java.util.L
 import com.example.demo.newentity.NewfileEntity;
 
 public interface NewfileService {
     NewfileEntity savedata(NewfileEntity newfile);
     NewfileEntity getidval(Long id);
 
-    
+
 
 }
