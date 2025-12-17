17864659ba1f14ce10709224bf9d3161e044b1cb
bala-amypo <bala@amypo.in>
2025-12-17 08:07:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/newentity/NewfileEntity.java
+++ src/main/java/com/example/demo/entity/newentity/NewfileEntity.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.NotBlank;
 
 public class NewfileEntity {
     @Id
+    @GeneratedValue(statergy=GenerationType.IDENTITY)
     private Long id;
     @NotBlank(message = "should not contain spaces")
     private String name;
     @NotBlank(message = "no blank allowed")
+    @Email(message="Invalid format")
     private String email;
 }
