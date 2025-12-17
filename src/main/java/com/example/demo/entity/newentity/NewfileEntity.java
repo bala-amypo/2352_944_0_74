0d7572da0108da6c1973da436fca320ae9f76b07
bala-amypo <bala@amypo.in>
2025-12-17 08:17:13 +0000

    User commit

--- src/main/java/com/example/demo/entity/newentity/NewfileEntity.java
+++ src/main/java/com/example/demo/entity/newentity/NewfileEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+@Entity
 
 public class NewfileEntity {
     @Id
@@ -16,5 +17,41 @@ public class NewfileEntity {
     private String name;
     @NotBlank(message = "no blank allowed")
     @Email(message="Invalid format")
-    private String email;
+    private String email;public NewfileEntity(
+            Long id,
+            @NotBlank(message = "should not contain spaces") String name,
+            @NotBlank(message = "no blank allowed")
+            @Email(message = "invalid format") String email) {
+
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public NewfileEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
