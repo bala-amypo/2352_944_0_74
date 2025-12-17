2b6bf318f5d2c86c16a2445f9ae64ec9704d40f4
bala-amypo <bala@amypo.in>
2025-12-17 10:42:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/newentity/NewfileEntity.java
+++ src/main/java/com/example/demo/entity/newentity/NewfileEntity.java
@@ -15,12 +15,11 @@ public class NewfileEntity {
     @GeneratedValue(statergy=GenerationType.IDENTITY)
     private Long id;
     @NotBlank(message = "should not contain spaces")
-    @column(unique=true)=
-    private String name;
+    @column(unique=true)=private String name;
     @NotBlank(message = "no blank allowed")
     @Email(message="Invalid format")
-    private String email;public NewfileEntity(Long id,@NotBlank(message = "should not contain spaces") String name,
-            @NotBlank(message = "no blank allowed") @Email(message = "invalid format") String email) {
+    private String email;
+    public NewfileEntity(Long id,@NotBlank(message = "should not contain spaces") String name, @NotBlank(message = "no blank allowed") @Email(message = "invalid format") String email) {
 
         this.id = id;
         this.name = name;
