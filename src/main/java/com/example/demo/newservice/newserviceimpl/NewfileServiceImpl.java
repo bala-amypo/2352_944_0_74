a8ef2af188850f73bde19347d956ec8a360b256c
bala-amypo <bala@amypo.in>
2025-12-17 10:37:12 +0000

    User commit

--- src/main/java/com/example/demo/newservice/newserviceimpl/NewfileServiceImpl.java
+++ src/main/java/com/example/demo/newservice/newserviceimpl/NewfileServiceImpl.java
@@ -1,52 +1,35 @@
-
 package com.example.demo.newservice.newserviceimpl;
+import java.util.*;
 
-import java.util.List;
 
-import org.springframework.stereotype.Service;
+public class NewserviceImpl implements Newservice {
+private final NewfileRepo rep;
+public NewserviceImpl(NewfileRepo rep){
+    this.rep = rep;
+}
+@Override
+public Newfile savedata (Newfile newww){
+    return rep.save(newww);
+}
+@Override
+public Newfile getidval(Long id){
+    return rep.findById(id);
+}
+@Override
+public Newfile getidval(Long id){
+    return rep.findAll();
 
-import com.example.demo.newentity.NewfileEntity;
-import com.example.demo.newrepository.NewfileRepo;
-import com.example.demo.newservice.NewfileService;
-@Service
-public class NewfileServiceImpl implements NewfileService {
-
-    private final NewfileRepo rep;
-
-    public NewfileServiceImpl(){
-        this.rep=rep;
-    }
-    @Override
-    public NewfileEntity savedata(NewfileEntity newfile) {
-        return rep.save(newfile);
-        
-    }
-
-    @Override
-    public NewfileEntity getidval(Long id) {
-        return rep.findById(id);
-        
-    }
-
-    @Override
-    public List<NewfileEntity> getall() {
-        return rep.findAll();
-       
-    }
-
-    @Override
-    public NewfileEntity update(Long id, NewfileEntity newfile) {
-        NewfileEntity existing =getidval(id);
-        existing.setName(newfile.getName());
-        existing.setEmail(newfile.getEmail());
-        return rep.save(existing);
-       
-    }
-    @Override
-public void delete(Long id) {
-    
-    rep.delete(Long id);
-    
+}
+@Override
+public Newfile update(Long id,Newfile newww){
+    Newfile existing = getidval(id);
+    existing.setName(newww.getName());
+    existing.setEmail(newww.getEmail());
+    return rep.save(existing);
+}
+@Override
+public void delete(Long id){
+    rep.delete(id);
 }
 
 }
