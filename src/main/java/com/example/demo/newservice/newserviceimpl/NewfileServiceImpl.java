72bd8240e909bc9f1f2731917f7429fca050f2e9
bala-amypo <bala@amypo.in>
2025-12-18 04:05:20 +0000

    User commit

--- src/main/java/com/example/demo/newservice/newserviceimpl/NewfileServiceImpl.java
+++ src/main/java/com/example/demo/newservice/newserviceimpl/NewfileServiceImpl.java
@@ -1,35 +1,41 @@
 package com.example.demo.newservice.newserviceimpl;
-import java.util.*;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.example. demo.newentity. NewFileEntity;
+import com.example.demo.newservice.NewFileService;
+@Service
+public class NewFileServiceImpl implements NewFileService{
+    
+    private final NewFileRepo rep;
+    @Override
 
+    public NewFileServiceImpl(NewFileRepo){
+        this.rep=rep;
+    }
+    public NewFileEntity savedata(NewFileEntity newfile) {
+        return rep.save(newfile);
 
-public class NewserviceImpl implements Newservice {
-private final NewfileRepo rep;
-public NewserviceImpl(NewfileRepo rep){
-    this.rep = rep;
-}
-@Override
-public Newfile savedata (Newfile newww){
-    return rep.save(newww)
-}
-@Override
-public Newfile getidval(Long id){
-    return rep.findById(id);
-}
-@Override
-public Newfile getidval(Long id){
-    return rep.findAll();
+    }
 
-}
-@Override
-public Newfile update(Long id,Newfile newww){
-    Newfile existing = getidval(id);
-    existing.setName(newww.getName());
-    existing.setEmail(newww.getEmail());
-    return rep.save(existing);
-}
-@Override
-public void delete(Long id){
-    rep.delete(id);
-}
+    @Override
+    public NewFileEntity getidval(Long id) {
+        return rep.findById(id);
+    }
+    @Override
+    public List<NewFileEntity> getall() {
+        return rep.findAll();
+    }
+ 
+    @Override
+    public NewFileEntity update(Long id, NewFileEntity newfile) {
+        NewfileEntity existing = getidval(id);
+        existing.setName(newfile.getName());
+        existing.setEmail(newfile.getEmail());
+        return rep.save(existing);
+    }
 
+    @Override
+    public void delete(Long id){
+        rep.delete(Long id);
+    }
 }
