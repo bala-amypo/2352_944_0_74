43b6fc06e67b1425c2a72129df2ae22908c98481
bala-amypo <bala@amypo.in>
2025-12-18 04:20:19 +0000

    User commit

--- src/main/java/com/example/demo/newservice/newserviceimpl/NewfileServiceImpl.java
+++ src/main/java/com/example/demo/newservice/newserviceimpl/NewfileServiceImpl.java
@@ -1,41 +1,52 @@
 package com.example.demo.newservice.newserviceimpl;
-import java.util.List;
-import org.springframework.stereotype.Service;
-import com.example. demo.newentity. NewFileEntity;
-import com.example.demo.newservice.NewFileService;
-@Service
-public class NewFileServiceImpl implements NewFileService{
-    
-    private final NewFileRepo rep;
-    @Override
 
-    public NewFileServiceImpl(NewFileRepo){
-        this.rep=rep;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.newentity.NewFileEntity;
+import com.example.demo.newrepo.NewFileRepo;
+import com.example.demo.newservice.NewFileService;
+
+@Service
+public class NewFileServiceImpl implements NewFileService {
+
+    private final NewFileRepo rep;
+
+    @Autowired
+    public NewFileServiceImpl(NewFileRepo rep) {
+        this.rep = rep;
     }
+
+    @Override
     public NewFileEntity savedata(NewFileEntity newfile) {
         return rep.save(newfile);
-
     }
 
     @Override
     public NewFileEntity getidval(Long id) {
-        return rep.findById(id);
+        return rep.findById(id).orElse(null);
     }
+
     @Override
     public List<NewFileEntity> getall() {
         return rep.findAll();
     }
- 
+
     @Override
     public NewFileEntity update(Long id, NewFileEntity newfile) {
-        NewfileEntity existing = getidval(id);
-        existing.setName(newfile.getName());
-        existing.setEmail(newfile.getEmail());
-        return rep.save(existing);
+        NewFileEntity existing = getidval(id);
+        if (existing != null) {
+            existing.setName(newfile.getName());
+            existing.setEmail(newfile.getEmail());
+            return rep.save(existing);
+        }
+        return null;
     }
 
     @Override
-    public void delete(Long id){
-        rep.delete(Long id);
+    public void delete(Long id) {
+        rep.deleteById(id);
     }
 }
